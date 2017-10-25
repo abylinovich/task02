6d9633554582e9ec00b38c2079a9d7e86e8ee3de
abylinovich <anddew@mail.ru>
2017-10-25 20:16:48 +0300

    Apply Maven

---
+++ src/main/java/by/tc/task02/entity/Node.java
@@ -0,0 +1,12 @@
+package by.tc.task02.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+
+    private String name;
+    private List<Node> nodeList = new ArrayList<>();
+
+
+}
---
+++ src/main/java/by/tc/task02/main/Main.java
@@ -0,0 +1,9 @@
+package by.tc.task02.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
